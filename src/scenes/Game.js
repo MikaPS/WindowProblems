118771d8d4279cbf80603231db17121c6bf4a2b2
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-05-17 13:03:25 -0700

    merge (#3)
    * added collision logic
    
    * fixed world bounds?
    
    * Adding sound effects
    
    ---------
    
    Co-authored-by: MikaPS <mikaps7@gmail.com>
    Co-authored-by: Ichiro Sugimoto <isugimot@ucsc.edu>

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,29 +6,21 @@ class Game extends Phaser.Scene {
   preload() {
     //Used from https://www.deviantart.com/rawen713/art/Graph-Paper-150326645
     this.load.image("background", "assets/img/graphpaper_background.jpg");
+
+    this.load.audio("paper1", "assets/sound/106127__j1987__crumple.wav");
+    this.load.audio("paper2", "assets/sound/508597__drooler__crumple-06.ogg");
+    this.load.audio("paper3", "assets/sound/540262__zepurple__uncrumpling-paper.wav");
+    this.load.audio("destruction", "assets/sound/641486__duskbreaker__8bit-explosion.wav");
+    this.load.audio("spawn", "assets/sound/476840__jackyyang09__paper-scrawling.wav");
   }
 
   create() {
-    /*
-    Tried working on Matter.js
-    const Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
-
-    // create an engine
-    const engine = Engine.create();
-
-    var render = Render.create({
-      element: document.body,
-      engine: engine,
-      options: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        wireframes: false
-      }
-    });
-    */
+    this.P_sound = false;
+    this.paper1 = this.sound.add("paper1");
+    this.paper2 = this.sound.add("paper2");
+    this.paper3 = this.sound.add("paper3");
+    this.destruction = this.sound.add("destruction");
+    this.spawn = this.sound.add("spawn");
     // window physics by lyssa
     game.settings = { centerX: 0, centerY: 0, cache: { x: 0, y: 0 } }; // for centering camera later
     this.add.image(0, 0, "background").setOrigin(0); // sample background image
@@ -56,54 +48,27 @@ class Game extends Phaser.Scene {
     this.updateScreenLocation();
 
     // matter physics by mika
-    this.graphics = this.add.graphics(); // only to draw the grid
-    // this.objectsGroup = this.add.group();
-    // this.add
-    //   .text(
-    //     400,
-    //     50,
-    //     "Right-click next to objects for explosion.\nLeft click the object on the spring to throw it."
-    //   )
-    //   .setOrigin(0.5);
-
-    // adding random boxes to the scene
-    // let box1 = this.matter.add.rectangle(300, 450, 50, 50);
-    // let box2 = this.matter.add.rectangle(500, 450, 50, 50);
-
-    this.matter.world.on("collisionstart", (event, bodyA, bodyB) => {
-      // bodyA.gameObject.setTint(0xff0000);
-      // bodyB.gameObject.setTint(0x00ff00);
-      // console.log("collision: ", bodyA.label, bodyB.label);
-      if (bodyA.label != "Rectangle Body" && bodyB.label != "Rectangle Body") {
-        // console.log("actual coliision", bodyA.label, bodyB.label);
-        // find the correct direction for explosion
-        let directionX = bodyA.position.x - bodyB.position.x;
-        let directionY = bodyA.position.y - bodyB.position.y;
-        console.log(bodyA.position, bodyB.position);
-        if (directionX < 0) {
-          directionX = -0.1;
-        } else {
-          directionX = 0.1;
+    // Chaos score text
+    this.explodedObjects = 0;
+    this.explodedText = this.add.text(
+      game.config.width * 0.85,
+      game.config.height * 0.05,
+      "Chaos Score:\n" + this.explodedObjects
+    );
+    this.explodedText.setColor("#000000");
+    // Explosion when two objects collide
+    this.matter.world.on("collisionstart", (event) => {
+      event.pairs.forEach((pair) => {
+        let bodyA = pair.bodyA;
+        let bodyB = pair.bodyB;
+        // Makes sure we dont collide with the world bounds
+        if (
+          bodyA.label != "Rectangle Body" &&
+          bodyB.label != "Rectangle Body"
+        ) {
+          this.collisionExplosion(bodyA, bodyB);
         }
-        if (directionY < 0) {
-          directionY = -0.1;
-        } else {
-          directionY = 0.1;
-        }
-        // Apply force to the body
-        let magnitude = 1000;
-        this.matter.applyForce(bodyB, {
-          x: directionX * magnitude,
-          y: directionY * magnitude,
-        });
-        console.log("force: ", directionX * magnitude, directionY * magnitude);
-        // this.matter.applyForce(bodyB, {
-        //   x: directionX * magnitude,
-        //   y: directionY * magnitude,
-        // });
-        // this.matter.world.remove(bodyA);
-        // this.matter.world.remove(bodyB);
-      }
+      });
     });
 
     // mouse movements
@@ -114,13 +79,14 @@ class Game extends Phaser.Scene {
       "pointerdown",
       function (pointer, event) {
         if (pointer.leftButtonDown()) {
-          this.crashingObjects(game.config.width, game.config.height);
+          // this.crashingObjects(game.config.width, game.config.height);
           this.airDropObjects(
             window.screenLeft,
             window.screenLeft + game.config.width,
             window.screenTop,
             window.screenTop + game.config.height
           );
+          this.spawn.play();
         }
       },
       this
@@ -145,26 +111,7 @@ class Game extends Phaser.Scene {
                 pointer.y
               ) < explosionRadius
             ) {
-              // find the correct direction for explosion
-              let directionX = body.position.x - pointer.x;
-              let directionY = body.position.y - pointer.y;
-
-              if (directionX < 0) {
-                directionX = -0.1;
-              } else {
-                directionX = 0.1;
-              }
-              if (directionY < 0) {
-                directionY = -0.1;
-              } else {
-                directionY = 0.1;
-              }
-              // Apply force to the body
-              let magnitude = 1;
-              this.matter.applyForce(body, {
-                x: directionX * magnitude,
-                y: directionY * magnitude,
-              });
+              this.collisionExplosion(body, body);
             }
           }
         }
@@ -178,11 +125,25 @@ class Game extends Phaser.Scene {
   // https://github.com/nathanaltice/CameraLucida/blob/master/src/scenes/FixedController.js
   update() {
     this.updateScreenLocation();
+    this.updateTextLocation();
+    this.explodedText.text = "Chaos Score:\n" + this.explodedObjects;
+
     // https://www.w3schools.com/jsref/prop_win_screentop.asp
     // console.log(`
     //     top: ${window.screenTop}
     //     left: ${window.screenLeft}
     // `);
+    if(this.paper1.isPlaying || this.paper2.isPlaying || this.paper3.isPlaying){
+      this.P_sound = true;
+    } else {
+      this.P_sound = false;
+    }
+  }
+
+  // Updated chaos score
+  updateTextLocation() {
+    this.explodedText.x = game.config.width * 0.85;
+    this.explodedText.y = game.config.height * 0.05;
   }
 
   // helper functions for window
@@ -191,8 +152,8 @@ class Game extends Phaser.Scene {
       game.settings.cache.x !== window.screenX &&
       game.settings.cache.y !== window.screenY
     ) {
-      console.log("screen moved");
-      console.log(game.settings.cache, window.screenX, window.screenY);
+      // console.log("screen moved");
+      // console.log(game.settings.cache, window.screenX, window.screenY);
       game.settings.cache = { x: window.screenX, y: window.screenY };
       game.settings.centerX = game.config.width / 2 + window.screenX;
       game.settings.centerY = game.config.height / 2 + window.screenY;
@@ -207,9 +168,17 @@ class Game extends Phaser.Scene {
     this.updateWorldBounds();
   }
   updateWorldBounds() {
+    var i = Phaser.Math.Between(1, 3);
+    if(i == 1 && this.P_sound == false){
+      this.paper1.play();
+    } else if(i == 2 && this.P_sound == false){
+      this.paper2.play();
+    } else if (i == 3 && this.P_sound == false){
+      this.paper3.play();
+    }
     this.matter.world.setBounds(
-      window.screenLeft,
-      window.screenTop,
+      game.settings.cache.x,
+      game.settings.cache.y,
       game.config.width,
       game.config.height,
       1000
@@ -222,77 +191,46 @@ class Game extends Phaser.Scene {
     let size = game.config.width / 50 + game.config.height / 50;
     let x = Math.random() * (endX - startX) + startX;
     let y = Math.random() * (endY - startY) + startY;
-    this.matter.add.polygon(x, y, side, size /*, {
-      render: {
-        strokeStyle: 'black',
-        lineWidth: 4
-      }
-    }*/);
-    // this.objectsGroup.add(this.b);
-    // this.objectsGroup.forEach(function (item) {
-    //   console.log("objects: ", item);
-    // });
+    let body = this.matter.add.polygon(x, y, side, size);
+    let colors = [0xff0000, 0x00ff00, 0x0000ff];
+    let color = Math.floor(Math.random() * colors.length);
+    this.matter.world.setBodyRenderStyle(body, colors[color], 1, 2);
   }
 
-  crashingObjects(startX, endX, startY, endY) {
-    // for (let k = 0; k < this.matter.world.localWorld.bodies.length; k++) {
-    //   for (let k = 0; k < this.matter.world.localWorld.bodies.length; k++) {
-    //   }
-    // }
+  collisionExplosion(bodyA, bodyB) {
+    // find the correct direction for explosion
+    let directionX = bodyA.position.x - bodyB.position.x;
+    let directionY = bodyA.position.y - bodyB.position.y;
+
+    if (directionX < 0) {
+      directionX = -0.1;
+    } else {
+      directionX = 0.1;
+    }
+    if (directionY < 0) {
+      directionY = -0.1;
+    } else {
+      directionY = 0.1;
+    }
+    // apply force to the body
+    let magnitude = 1;
+
+    // introducing a delay between the explosion and destroying the object
+    setTimeout(() => {
+      this.updated = false;
+      this.matter.applyForce(bodyB, {
+        x: directionX * magnitude,
+        y: directionY * magnitude,
+      });
+
+      setTimeout(() => {
+        this.matter.world.remove(bodyB);
+        if (this.updated == false) {
+          this.updated = true;
+          this.explodedObjects += 1;
+          this.destruction.play();
+        }
+      }, 2000);
+    }, 50);
   }
-  //   let gridSize = 4;
-  //   let maxAllowed = 3;
-  //   let areaWidth = maxX / gridSize;
-  //   let areaHeight = maxY / gridSize;
-
-  //   // go through each grid cell
-  //   for (let i = 0; i < gridSize; i++) {
-  //     for (let j = 0; j < gridSize; j++) {
-  //       // draws the grid (FOR DEBUGGING)
-  //       this.graphics.strokeRect(
-  //         areaWidth * i,
-  //         areaHeight * j,
-  //         areaWidth,
-  //         areaHeight
-  //       );
-
-  //       let count = 0;
-
-  //       // go through all bodies in the world
-  //       for (let k = 0; k < this.matter.world.localWorld.bodies.length; k++) {
-  //         let body = this.matter.world.localWorld.bodies[k];
-  //         // check if it's in a specific area and add it to a count
-  //         if (
-  //           body.position.x > areaWidth * i &&
-  //           body.position.x < areaWidth * i + areaWidth &&
-  //           body.position.y > areaHeight * j &&
-  //           body.position.y < areaHeight * j + areaHeight
-  //         ) {
-  //           count++;
-  //         }
-  //       }
-
-  //       // check if there are too many bodies
-  //       if (count >= maxAllowed) {
-  //         for (let k = 0; k < this.matter.world.localWorld.bodies.length; k++) {
-  //           let body = this.matter.world.localWorld.bodies[k];
-  //           // delete all the over crowding bodies
-  //           if (
-  //             body.position.x > areaWidth * i &&
-  //             body.position.x < areaWidth * i + areaWidth &&
-  //             body.position.y > areaHeight * j &&
-  //             body.position.y < areaHeight * j + areaHeight
-  //           ) {
-  //             if (body != this.ground) {
-  //               this.matter.world.remove(body);
-  //             }
-  //           }
-  //         }
-
-  //         return true;
-  //       }
-  //     }
-  //   }
-  //   return false;
-  // }
 }
